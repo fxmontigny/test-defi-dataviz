fbb41d8651dba9d14c4f12ebd15f0d5db4eda94c
François-Xavier Montigny <francois-xavier@delivreetic.com>
2021-10-01 15:42:55 +0200

    fix build file

--- server.js
+++ server.js
@@ -3,23 +3,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-
-const redirectohttps = true;
-
 app.use(compression());
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/test-defi-dataviz'));
 app.get('/*', function(req, res) {
-  if (redirectohttps && req.headers['x-forwarded-proto'] !== 'https' && req.hostname !== 'localhost') {
-    // special for robots.txt
-    if (req.url === '/robots.txt') {
-      next();
-
-      return;
-    }
-
-    res.redirect(301, 'https://' + req.hostname + req.url);
-  }
-
-	res.sendFile(path.join(__dirname + '/dist/index.html'));
+	res.sendFile(path.join(__dirname + '/dist/test-defi-dataviz/index.html'));
 });
 app.listen(process.env.PORT || 3000);
